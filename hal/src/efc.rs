83de6117f0f097f0370e6a267e64179aa60bb8ff
Martin Mörtsell <martin.mortsell@grepit.se>
2022-04-26 07:51:26 -0700

    Fix Return Types

--- hal/src/efc.rs
+++ hal/src/efc.rs
@@ -46,10 +46,11 @@ impl TryFrom<u8> for FlashWaitStates {
     type Error = PmcError;
 
     #[cfg(feature = "vddio-3v")]
-    fn try_from(freq: u8) -> Result<Self, Error> {
+    fn try_from(freq: u8) -> Result<Self, Self::Error> {
         // References:
         // - Table 58-50 (p. 1804) Embedded Flash Wait States for Worst-Case Conditions (V70/V71)
         // - Table 59-50 (p. 1850) Embedded Flash Wait States for Worst-Case Conditions (E70/S70; VDDIO = 3.0V)
+        Ok(
         match freq {
             0..=23 => Self::Zero,
             24..=46 => Self::One,
@@ -58,14 +59,15 @@ impl TryFrom<u8> for FlashWaitStates {
             93..=115 => Self::Four,
             116..=138 => Self::Five,
             139..=150 => Self::Six,
-            _ => Err(PmcError::InvalidConfiguration),
-        }
+            _ => return Err(PmcError::InvalidConfiguration),
+        })
     }
 
     #[cfg(feature = "vddio-1v")]
-    fn try_from(freq: u8) -> Result<Self, Error> {
+    fn try_from(freq: u8) -> Result<Self, Self::Error> {
         // References:
         // - Table 59-50 (p. 1850) Embedded Flash Wait States for Worst-Case Conditions (E70/S70; VDDIO = 1.7V)
+        Ok(
         match freq {
             0..=21 => Self::Zero,
             22..=42 => Self::One,
@@ -74,8 +76,9 @@ impl TryFrom<u8> for FlashWaitStates {
             85..=106 => Self::Four,
             107..=125 => Self::Five,
             126..=137 => Self::Six,
-            _ => Err(PmcError::InvalidConfiguration),
+            _ => return Err(PmcError::InvalidConfiguration),
         }
+        )
     }
 }
 
@@ -86,6 +89,6 @@ impl FlashWaitStates {
     /// The max mck frequency supported is 150MHz. This is *not* the CPU frequency,
     /// which may go up to 300MHz.
     pub fn from_mck_mhz(freq: u8) -> Result<Self, PmcError> {
-        freq.try_into()?
+        Ok(freq.try_into()?)
     }
 }
