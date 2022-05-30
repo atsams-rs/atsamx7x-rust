b88ffd6d53dac1d8ee2e134b9a7b65b60f5d3cbb
Viktor Sonesten <viktor.sonesten@grepit.se>
2022-05-30 14:52:56 +0200

    atsame70_xpro: apply example clock hierarchy configuration
    This will later be moved into a proper examples/*.rs when a GPIO API
    is available.

--- boards/atsame70_xpro/src/main.rs
+++ boards/atsame70_xpro/src/main.rs
@@ -21,6 +21,39 @@ mod app {
         // Disable the watchdog.
         hal::watchdog::Watchdog::new(ctx.device.WDT).disable();
 
+        let mut efc = {
+            use hal::efc::{Efc, VddioLevel};
+            Efc::new(ctx.device.EFC, VddioLevel::V3)
+        };
+
+        // Configure the clock hierarchy
+        {
+            use hal::pmc::{
+                HostClockConfig, MainCkSource, MckDivider, MckPrescaler, Megahertz, PckId,
+                PllaConfig, Pmc,
+            };
+
+            let mut pmc = Pmc::new(ctx.device.PMC);
+            let mainck = pmc
+                .get_mainck(MainCkSource::ExternalBypass(Megahertz::from_raw(12)))
+                .unwrap();
+            let plla = pmc
+                .get_pllack(PllaConfig { div: 1, mult: 8 }, &mainck)
+                .unwrap();
+            let _hclk = pmc
+                .get_hclk(
+                    HostClockConfig {
+                        pres: MckPrescaler::CLK_1,
+                        div: MckDivider::EQ_PCK,
+                    },
+                    &mainck,
+                    &mut efc,
+                )
+                .unwrap();
+
+            let _pck2 = pmc.get_pck(&plla, 0, PckId::Pck2);
+        }
+
         (Shared {}, Local {}, init::Monotonics())
     }
 
