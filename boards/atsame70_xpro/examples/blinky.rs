baf22cc58ec1ad9f7c67999e5a69355619e46786
Viktor Sonesten <viktor.sonesten@grepit.se>
2022-06-14 16:55:17 +0200

    atsame70_xpro: add blinky example

---
+++ boards/atsame70_xpro/examples/blinky.rs
@@ -0,0 +1,41 @@
+//! Periodically blinks the board's LED2 (located next to the barrel
+//! connector) at ~1Hz.
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[rtic::app(device = hal::target_device, peripherals = true)]
+mod app {
+    use atsamx7x_hal as hal;
+    use hal::ehal::{digital::v2::ToggleableOutputPin, watchdog::WatchdogDisable};
+    use hal::pio::*;
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {
+        led: Pin<PB8, Output>,
+    }
+
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // Disable the watchdog.
+        hal::watchdog::Watchdog::new(ctx.device.WDT).disable();
+
+        let mut pmc = hal::pmc::Pmc::new(ctx.device.PMC);
+        let bankb = hal::pio::BankB::new(ctx.device.PIOB, &mut pmc, BankConfiguration::default());
+        let led = bankb.pb8.into_output();
+
+        (Shared {}, Local { led }, init::Monotonics())
+    }
+
+    #[idle(local = [led])]
+    fn idle(ctx: idle::Context) -> ! {
+        loop {
+            ctx.local.led.toggle().unwrap();
+            cortex_m::asm::delay(12_000_000);
+        }
+    }
+}
